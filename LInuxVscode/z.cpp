baf597126e2753168900fb9a412d41df0df85022
FahadIbnaTuhin <fahadtuhin2@gmail.com>
2024-12-03 00:10:44 +0600

    updaet

---
+++ LInuxVscode/z.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    // ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    for (int i = 0; i < 5; i++) {
+        cout << i << '\n';
+    }
+
+
+    return 0;
+}
+
+// find . -maxdepth 1 -type f -executable ! -name '*.*' -exec rm -f {} +
